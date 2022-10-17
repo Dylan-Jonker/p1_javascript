cebe615c0a8ef4c026860d2f84cba5ebee2b7d8a
Dylan-Jonker <9017749@student.zadkine.nl>
2022-10-17 11:13:08 +0200

    7.1 en 7.3

--- opdracht_7.1/app.js
+++ opdracht_7.1/app.js
@@ -1,2 +1,12 @@
-let num= 100
-alert(num)
+let txt = document.getElementById("txt");
+
+let budget = 100;
+let product = prompt("Hoe veel kost het product");
+
+if (budget >= product) {
+    txt.innerText = "U heeft genoeg geld!";
+    txt.style.color = "green";
+} else {
+    txt.innerText = "Helaas, te weinig geldt"
+    txt.style.color = "red";
+}
